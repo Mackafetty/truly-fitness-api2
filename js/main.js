0f6a9e263e48237c5576fa47ea7f34f212d3eba0
Mackafetty <justin@thejcode.net>
2023-03-21 13:04:51 -0500

    added event listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const dayTarget = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://thoughtful-moth-cap.cyclic.app/api/${dayTarget}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data
+    }catch(error){
+        console.log(error)
+    }
+}
